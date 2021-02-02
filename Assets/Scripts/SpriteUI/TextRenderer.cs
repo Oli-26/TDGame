0b1ed9882956799191db21f5e5ae61dbfc9d5741
JakeDavison <jake.andrew.davison@gmail.com>
2021-02-02 00:46:50 +0000

    UI Updates to Jake Scene

---
+++ Assets/Scripts/SpriteUI/TextRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextRenderer : MonoBehaviour
+{
+
+    private static int sortOrder = 4; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] textObjects = GameObject.FindGameObjectsWithTag("text");
+        
+        foreach(GameObject obj in textObjects) {
+            obj.GetComponent<MeshRenderer>().sortingOrder = sortOrder;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

9ba287c7305dd3c328b64822581d310b2a2ad5ad
Oli-26 <Olivermholder@gmail.com>
2021-02-02 22:57:17 +0000

    pawns moving

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -49,7 +49,6 @@ public class BaseEnemy : MonoBehaviour
 
     public virtual void Move(){
         if(!nextFlagExists){
-            Debug.Log("flag not set, unable to move.");
             return;
         };
         moveBetween(transform.position, nextFlag.transform.position, 1f);
@@ -84,6 +83,7 @@ public class BaseEnemy : MonoBehaviour
     }
 
     public void OverRideInitalisationWithNewSpawn(Vector3 pos, GameObject flag){
+        pathScript = GameObject.FindWithTag("Path").GetComponent<Path>();
         initalFlagSet = true;
         transform.position = pos;
         SetLastFlag(flag);

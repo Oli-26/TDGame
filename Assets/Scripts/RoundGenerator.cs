56b71cacab6af3cb650ebe9c6ee0ca387a1ac444
Oli-26 <Olivermholder@gmail.com>
2021-02-09 18:28:10 +0000

    Rounds moved again to Resources folder

--- Assets/Scripts/RoundGenerator.cs
+++ Assets/Scripts/RoundGenerator.cs
@@ -13,40 +13,34 @@ public class RoundGenerator : MonoBehaviour
     public GameObject queenPrefab;
 
     public List<(int, GameObject)> readRoundFromFile(int round){
-        string tempPath = Application.dataPath + "/rounds/round" + (round.ToString()) +".txt";
-        Debug.Log(tempPath);
-        FileInfo t = new FileInfo(tempPath); 
         try{
-            if(t.Exists) 
-            { 
-                var _reader = t.OpenText(); 
-                string text = _reader.ReadToEnd();
-                string[] lines = text.Split('\n');
+            TextAsset textasset = (TextAsset)Resources.Load("rounds/round" + round.ToString());
+            string text = textasset.text;
+            string[] lines = text.Split('\n');
 
-                
-                List<(int, GameObject)> returnList = new List<(int, GameObject)>();
-                foreach(string line in lines){
-                    string[] values = line.Split(' ');
-                    int spawnAtTick = 6*int.Parse(values[0]);
-                    GameObject enemyToSpawn = prefabValueLookUp(int.Parse(values[1]));
-                    if(values.Length > 2){
-                        int amount = int.Parse(values[2]);
-                        int spawnSeperator = 1;
-                        if(values.Length > 3){
-                            spawnSeperator = int.Parse(values[3]);
-                        }
-                            for(int i = 0; i < amount; i++){
-                                (int, GameObject) pair = (spawnAtTick+i*spawnSeperator*6, enemyToSpawn);
-                                returnList.Add(pair);
-                            }
-                    }else{
-                        (int, GameObject) pair = (spawnAtTick, enemyToSpawn);
-                        returnList.Add(pair);
+            
+            List<(int, GameObject)> returnList = new List<(int, GameObject)>();
+            foreach(string line in lines){
+                string[] values = line.Split(' ');
+                int spawnAtTick = 6*int.Parse(values[0]);
+                GameObject enemyToSpawn = prefabValueLookUp(int.Parse(values[1]));
+                if(values.Length > 2){
+                    int amount = int.Parse(values[2]);
+                    int spawnSeperator = 1;
+                    if(values.Length > 3){
+                        spawnSeperator = int.Parse(values[3]);
                     }
+                        for(int i = 0; i < amount; i++){
+                            (int, GameObject) pair = (spawnAtTick+i*spawnSeperator*6, enemyToSpawn);
+                            returnList.Add(pair);
+                        }
+                }else{
+                    (int, GameObject) pair = (spawnAtTick, enemyToSpawn);
+                    returnList.Add(pair);
                 }
-                _reader.Close();
-                return returnList;
             }
+            return returnList;
+            
         }catch(Exception e){
             Debug.Log(e);
         }

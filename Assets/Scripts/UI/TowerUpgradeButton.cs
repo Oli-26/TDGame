657b01a1eb207d5d51417346aeebbf23224190a5
JakeDavison <jake.andrew.davison@gmail.com>
2021-02-18 21:29:04 +0000

    Added description box for upgrades.

--- Assets/Scripts/UI/TowerUpgradeButton.cs
+++ Assets/Scripts/UI/TowerUpgradeButton.cs
@@ -1,17 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class TowerUpgradeButton : Button {
+public class TowerUpgradeButton : Button, IPointerEnterHandler, IPointerExitHandler
+{
 
+    public GameObject upgradeDescriptionBox;
     public int upgrade = 0;
+    private Canvas myCanvas;
+    private bool mouseIsOver { get; set; }
+    
+    void Start()
+    {
+        myCanvas = GetComponentInParent<Canvas>();
+    }
+
+    protected void Update()
+    {
+        base.Update();
+        if (mouseIsOver)
+        {
+            Vector2 pos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(myCanvas.transform as RectTransform, Input.mousePosition, myCanvas.worldCamera, out pos);
+            upgradeDescriptionBox.transform.position = myCanvas.transform.TransformPoint(pos);
+        }
+    }
+    
 
     public override void clicked(){
         Debug.Log(upgrade + " clicked");
-        BaseTower tower = control.GetComponent<UiContoller>().SelectedTower;
-        List<TowerUpgrade> upgrades = tower.GetBuyableUpgrades();
-        if (control.GetComponent<Stats>().SpendMoney(upgrades.Find(u => u.track == upgrade).cost)) {
-            tower.BuyUpgrade(upgrades.Find(u => u.track == upgrade));
+        var towerUpgrade = GetAssociatedUpgrade();
+
+        if (control.GetComponent<Stats>().SpendMoney(towerUpgrade.cost)) {
+            GetSelectedTower().BuyUpgrade(towerUpgrade);
+            UpdateDescriptionBox();
         }
+    }
+
+    private TowerUpgrade GetAssociatedUpgrade()
+    {
+        List<TowerUpgrade> upgrades = GetSelectedTower().GetBuyableUpgrades();
+        TowerUpgrade towerUpgrade = upgrades.Find(u => u.track == upgrade);
+        return towerUpgrade;
+    }
+
+    private BaseTower GetSelectedTower()
+    {
+        return control.GetComponent<UiContoller>().SelectedTower;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        mouseIsOver = true;
+        UpdateDescriptionBox();
+        upgradeDescriptionBox.SetActive(true);
+    }
+
+    private void UpdateDescriptionBox()
+    {
+        upgradeDescriptionBox.transform.GetChild(0).gameObject.GetComponent<Text>().text =
+            GetAssociatedUpgrade().description;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        mouseIsOver = false;
+        upgradeDescriptionBox.SetActive(false);
     }
 }
